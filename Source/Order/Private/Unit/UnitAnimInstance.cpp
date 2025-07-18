4dcaeefe02e5a04216f899717ac95e03f175d609
kimeorua <kimeorua1222@naver.com>
2025-07-18 14:31:46 +0900

    animBP

--- Source/Order/Private/Unit/UnitAnimInstance.cpp
+++ Source/Order/Private/Unit/UnitAnimInstance.cpp
@@ -2,4 +2,14 @@
 
 
 #include "Unit/UnitAnimInstance.h"
+#include "Unit/BaseUnit.h"
 
+void UUnitAnimInstance::NativeInitializeAnimation()
+{
+	OwningUnit = Cast<ABaseUnit>(TryGetPawnOwner());
+}
+
+void UUnitAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	if (!OwningUnit) { return; }
+}
--- Source/Order/Public/Unit/UnitAnimInstance.h
+++ Source/Order/Public/Unit/UnitAnimInstance.h
@@ -6,12 +6,18 @@
 #include "Animation/AnimInstance.h"
 #include "UnitAnimInstance.generated.h"
 
-/**
- * 
- */
+class ABaseUnit;
+
 UCLASS()
 class ORDER_API UUnitAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds)override;
+
+protected:
+	UPROPERTY()
+	ABaseUnit* OwningUnit;
 };
